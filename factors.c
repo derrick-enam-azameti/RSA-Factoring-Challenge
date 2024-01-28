c8257398a08786a538559fe10237ecb2c4f2d87e
Derrick Enam Azameti <azametid@gmail.com>
2024-01-28 23:01:19 +0000

    factors

--- factors.c
+++ factors.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	long long number;
+	long long div;
+
+
+	if (argc != 2)
+	{
 		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	FILE *stream = fopen(argv[1], "r");
-	if (stream == NULL) {
+	stream = fopen(argv[1], "r");
+	if (stream == NULL)
+	{
 		perror("fopen");
 		exit(EXIT_FAILURE);
 	}
 
-	char *line = NULL;
-	size_t len = 0;
-
-	while (getline(&line, &len, stream) != -1) {
-		long long number = atoll(line);
-		if (number < 2) {
+	while (getline(&line, &len, stream) != -1)
+	{
+		number = atoll(line);
+		if (number < 2)
+		{
 			printf("%lld is not factorizable\n", number);
-		} else {
+		} else
+		{
 			printf("Prime factorization of %lld: ", number);
-			for (long long div = 2; div <= number; ++div) {
-				while (number % div == 0) {
+			for (div = 2; div <= number; ++div)
+			{
+				while (number % div == 0)
+				{
 					printf("%lld", div);
 					number /= div;
-					if (number > 1) {
+					if (number > 1)
+					{
 						printf("*");
 					}
 				}
